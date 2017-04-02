e64aebb9821f9aa1ea2a6ce4e2e4c066d330088f
remi <remi.coudert@epfl.ch>
2017-04-02 23:10:55 +0200

    some fixes

--- reactors-core/jvm/src/bench/scala/io/reactors/concurrent/QuickSortBench.scala
+++ reactors-core/jvm/src/bench/scala/io/reactors/concurrent/QuickSortBench.scala
@@ -1,5 +1,7 @@
 package io.reactors.concurrent
 
+
+
 import akka.actor.{Actor, ActorRef, ActorSystem, Props}
 import io.reactors.{Channel, Reactor, ReactorSystem}
 import org.scalameter.api.{MongoDbReporter, RegressionReporter}
@@ -7,9 +9,12 @@ import org.scalameter.{Context, Reporter}
 import org.scalameter.api._
 import org.scalameter.japi.JBench
 
+import scala.collection.mutable
 import scala.concurrent.{Await, Promise}
 import scala.concurrent.duration._
 import scala.util.Random
+
+
 
 /**
   * Created by remi on 27/03/17.
@@ -21,7 +26,7 @@ class QuickSortBench extends JBench.OfflineReport {
     exec.maxWarmupRuns -> 40,
     exec.benchRuns -> 48,
     exec.independentSamples -> 4,
-    verbose -> true
+    verbose -> false
   )
 
   override def reporter = Reporter.Composite(
@@ -29,42 +34,36 @@ class QuickSortBench extends JBench.OfflineReport {
     new MongoDbReporter[Double]
   )
 
-  val lists: Gen[List[Long]] = Gen.range("size")(1000000, 1000000, 1000000).map(QuickSortConfig.randomlyInitArray(_))
+  val lists: Gen[Array[Long]] = Gen
+    .exponential("size")(1, 1000000, 10)
+    .map(QuickSortConfig.arrayForSize(_))
 
   @transient lazy val system = new ReactorSystem("reactor-bench")
 
   @gen("lists")
   @benchmark("io.reactors.quicksort")
   @curve("onEvent")
-  def reactorOnEvent(list: List[Long]) = {
+  def reactorOnEvent(list: Array[Long]) = {
+    val done = Promise[Boolean]()
 
-    val done = Promise[List[Long]]()
-
-    def quicksort(parent: Channel[Message], positionRelativeToParent: Position): Channel[Message] = {
-
-      val ch = system.spawn(Reactor[Message] { self =>
-
-        var result = List.empty[Long]
+    def quicksort(parent: Channel[Message],
+                  positionRelativeToParent: Position): Channel[Message] = {
+      system.spawn(Reactor[Message] { self =>
+        var result = Array.emptyLongArray
         var numFragments = 0
 
-
-        def notifyParentAndTerminate() {
+        def notifyParentAndTerminate() = {
           parent ! ResultMessage(result, positionRelativeToParent)
           self.main.seal()
         }
 
         self.main.events.onMatch {
           case SortMessage(data) =>
-
-            val dataLength: Int = data.length
-            if (dataLength < QuickSortConfig.T) {
-
+            if (data.length < QuickSortConfig.T) {
               result = data.sorted
               notifyParentAndTerminate()
-
             } else {
-
-              val dataLengthHalf = dataLength / 2
+              val dataLengthHalf = data.length / 2
               val pivot = data(dataLengthHalf)
 
               val leftUnsorted = data.filter(_ < pivot)
@@ -81,17 +80,11 @@ class QuickSortBench extends JBench.OfflineReport {
             }
 
           case ResultMessage(data, position) =>
-
             if (data.nonEmpty) {
-
               if (position eq PositionLeft) {
-
                 result = data ++ result
-
               } else if (position eq PositionRight) {
-
-                result = data ++ result
-
+                result = result ++ data
               }
             }
 
@@ -102,16 +95,17 @@ class QuickSortBench extends JBench.OfflineReport {
         }
 
       })
-      ch
     }
 
     system.spawn(Reactor[Message] { self =>
       self.main.events.onEvent {
-        case ResultMessage(l, PositionInitial) => {
+        case ResultMessage(l, PositionInitial) =>
           val sorted = QuickSortConfig.checkSorted(l.length, l)
-          if(sorted) done.success(l)
-          else done.failure(new RuntimeException("List isn't sorted"))
-        }
+          if(sorted) {
+            self.main.seal()
+            done.success(sorted)
+          }
+          else throw new RuntimeException("Array isn't sorted - reactor")
       }
       quicksort(self.main.channel, PositionInitial) ! SortMessage(list)
     })
@@ -134,80 +128,89 @@ class QuickSortBench extends JBench.OfflineReport {
   @curve("akka")
   @setupBeforeAll("akkaSetup")
   @teardownAfterAll("akkaTeardown")
-  def akka(l: List[Long]): Unit = {
-
-    val done = Promise[List[Long]]()
-
+  def akka(l: Array[Long]): Unit = {
+    val done = Promise[Boolean]()
     actorSystem.actorOf(
       Props.create(classOf[QuickSortRootActor], done, l))
-
     Await.ready(done.future, 10.seconds)
   }
-
-
 }
 
-protected object QuickSortConfig {
 
+protected object QuickSortConfig {
   private val M: Long = 1L << 60 // max value
   val T = 2048 // threshold to perform sort sequentially
   private val S = 1024 // seed for random number generator
 
- def checkSorted(N: Long, data: List[Long]): Boolean = {
+ def checkSorted(N: Long, data: Array[Long]): Boolean = {
    val length = data.length
-
    if (length != N) {
      return false
    }
-
-   data.zip(data.tail).forall(x => x._1 <= x._2)
-
+   data.sorted sameElements data
   }
 
- def randomlyInitArray(N: Long): List[Long] = {
+  val inputArrays: mutable.Map[Long, Array[Long]] =
+    mutable.Map.empty[Long, Array[Long]]
 
-   val l = List.newBuilder[Long]
+  def arrayForSize(N: Long): Array[Long] = inputArrays.getOrElseUpdate(N, randomlyInitArray(N))
 
+  def randomlyInitArray(N: Long): Array[Long] = {
+   val l = Array.newBuilder[Long]
    val random = new Random(S)
-
    (0L until N).foreach{_ => l += Math.abs(random.nextLong() % M)}
-
    l.result()
   }
-
 }
+
 
 abstract class Position
 
+
 case object PositionRight extends Position
+
 
 case object PositionLeft extends Position
 
+
 case object PositionInitial extends Position
+
 
 abstract class Message
 
-case class SortMessage(data: List[Long]) extends Message
 
-case class ResultMessage(data: List[Long], position: Position) extends Message
+case class SortMessage(data: Array[Long])
+  extends Message
 
-class QuickSortRootActor(done: Promise[List[Long]], list: List[Long]) extends Actor {
+
+case class ResultMessage(data: Array[Long], position: Position)
+  extends Message
+
+
+class QuickSortRootActor(done: Promise[Boolean], list: Array[Long])
+  extends Actor {
   override def preStart() {
-    val child = context.actorOf(Props.create(classOf[QuickSortActor], self, PositionInitial))
+    val child = context.actorOf(
+      Props.create(classOf[QuickSortActor], self, PositionInitial)
+    )
     child ! SortMessage(list)
   }
   def receive: Receive = {
     case ResultMessage(l, PositionInitial) => {
       val sorted = QuickSortConfig.checkSorted(l.length, l)
-      if(sorted) done.success(l)
-      else done.failure(new RuntimeException("List isn't sorted"))
+      if(sorted) {
+        done.success(sorted)
+        context.stop(self)
+      }
+      else throw new RuntimeException("Array isn't sorted - actor")
     }
   }
 }
 
-class QuickSortActor(parent: ActorRef, positionRelativeToParent: Position) extends Actor {
 
-  private var result = List.empty[Long]
+class QuickSortActor(parent: ActorRef, positionRelativeToParent: Position)
+  extends Actor {
+  private var result = Array.emptyLongArray
   private var numFragments = 0
 
   def notifyParentAndTerminate() {
@@ -217,15 +220,11 @@ class QuickSortActor(parent: ActorRef, positionRelativeToParent: Position) exten
 
   override def receive: Receive = {
       case SortMessage(data) =>
-
         val dataLength: Int = data.length
         if (dataLength < QuickSortConfig.T) {
-
           result = data.sorted
           notifyParentAndTerminate()
-
         } else {
-
           val dataLengthHalf = dataLength / 2
           val pivot = data(dataLengthHalf)
 
@@ -233,27 +232,25 @@ class QuickSortActor(parent: ActorRef, positionRelativeToParent: Position) exten
           val rightUnsorted = data.filter(_ > pivot)
           result = data.filter(_ == pivot)
 
-          val leftActor = context.system.actorOf(Props.create(classOf[QuickSortActor], self, PositionLeft))
+          val leftActor = context.system.actorOf(
+            Props.create(classOf[QuickSortActor], self, PositionLeft)
+          )
           leftActor ! SortMessage(leftUnsorted)
 
-          val rightActor = context.system.actorOf(Props.create(classOf[QuickSortActor], self, PositionRight))
+          val rightActor = context.system.actorOf(
+            Props.create(classOf[QuickSortActor], self, PositionRight)
+          )
           rightActor ! SortMessage(rightUnsorted)
 
           numFragments += 1
         }
 
       case ResultMessage(data, position) =>
-
         if (data.nonEmpty) {
-
           if (position eq PositionLeft) {
-
             result = data ++ result
-
           } else if (position eq PositionRight) {
-
-            result = data ++ result
-
+            result ++= data
           }
         }
 
@@ -263,4 +260,3 @@ class QuickSortActor(parent: ActorRef, positionRelativeToParent: Position) exten
         }
   }
 }
-
